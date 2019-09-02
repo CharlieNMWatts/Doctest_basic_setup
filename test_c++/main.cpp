6c8bc9f52e13987f627383ccef340c6aad708f3b
Charlie watts <a1717797@student.adelaide.edu.au>
2019-09-03 01:40:08 +0930

    Initial Commit

---
+++ test_c++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test_c++
+//
+//  Created by Charlie Watts on 3/9/19.
+//  Copyright © 2019 Charlie Watts. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
